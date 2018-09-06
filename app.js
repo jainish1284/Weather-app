920b2b33cb58d032ba20b6636d5b9921def6cb7d
jainish patel <jainish.1284.jp@gmail.com>
2018-09-06 18:31:46 +0530

    Commit

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+        .options({
+            a: {
+                demand: true,
+                alias: 'address',
+                describe: 'This is Address',
+                string: true
+            }
+        })
+        .argv;
+
+var address = encodeURIComponent(argv.a);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
     json: true
 },(error,response,body)=>{
     console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
 });
